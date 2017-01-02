f9ed8bce1c800a1d78d195cabb7a06776b000e24
Gerald Versluis <gerald@verslu.is>
2017-01-02 08:59:33 +0100

    Updated image link for broken URL

--- FreshMvvm.Sample/FreshMvvm.Sample/PageModels/TabTwoPageModel.cs
+++ FreshMvvm.Sample/FreshMvvm.Sample/PageModels/TabTwoPageModel.cs
@@ -5,21 +5,16 @@ namespace FreshMvvm.Sample.PageModels
 {
     public class TabTwoPageModel : FreshBasePageModel
     {
-        public string UnicornText
-        {
-            get
-            {
+        public string UnicornText {
+            get {
                 return "unicorn!";
             }
         }
 
-        public Command ShowUnicornCommand
-        {
-            get
-            {
-                return new Command(() =>
-                {
-                    CoreMethods.PushPageModel<UnicornPageModel>("http://verslu.is/img/assets/logo-white.png", true);
+        public Command ShowUnicornCommand {
+            get {
+                return new Command (() => {
+                    CoreMethods.PushPageModel<UnicornPageModel> ("https://gerald.verslu.is/assets/img/Logo_nobg.png", true);
                 });
             }
         }
